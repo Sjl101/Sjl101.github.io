0c199d64e1db4f9ea5e8bced977738c61ab96a6f
Sjl101 <shanejordanlawson@sky.com>
2019-01-08 20:53:43 +0000

    Update 1
    Update 1

---
+++ JS/code.js
@@ -0,0 +1,63 @@
+
+function load() {
+    var slideIndex = 1;
+    showSlides(slideIndex)
+}
+function geoFindMe() {
+    var output = document.getElementById("out");
+  
+    if (!navigator.geolocation){
+      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+      return;
+    }
+  
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      output.innerHTML = '<p>Latitude is ' + latitude + ' <br>Longitude is ' + longitude + '</p>';
+    }
+  
+    function error() {output.innerHTML = "Unable to retrieve your location";}
+  
+    output.innerHTML = "Locating...";
+  
+    navigator.geolocation.getCurrentPosition(success, error);
+    
+  }
+var username = localStorage.getItem("username")
+var name = document.getElementById("un");
+function NameCheck() {
+  if(!username){
+    username = prompt("What shall we call you?");
+    localStorage.setItem("username", username);
+    name.innerHTML = "Welcome Back" + username;
+  }
+  else{name.innerHTML = "Welcome Back" + username;}
+}
+
+function updatename() {
+  var username = localStorage.getItem("username");
+  var name = document.getElementById("un");
+  name.innerHTML = "Welcome Back " + username;
+  {document.getElementById("namebutton").style.display="none";}
+  {document.getElementById("un").style.display="inline";}
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {showSlides(slideIndex += n);}
+
+function currentSlide(n) {showSlides(slideIndex = n);}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {slides[i].style.display = "none";}
+  for (i = 0; i < dots.length; i++) {dots[i].className = dots[i].className.replace(" active", "");}
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
